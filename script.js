50c5dabb39bae757ec1c0aee9bd10f72f1484fa4
Luisa <luisa.garcia1331@gmail.com>
2023-01-23 19:34:36 -0500

    reubicacion codigo de js y aumento de funcionalidades

---
+++ script.js
@@ -0,0 +1,48 @@
+function encriptar(){
+    var textoEntrada = document.getElementById("textoIngreso").value.toLowerCase();
+    var textoEncriptado = textoEntrada.replace(/e/img, "enter");
+    var textoEncriptado = textoEncriptado.replace(/i/img, "imes");
+    var textoEncriptado = textoEncriptado.replace(/a/img, "ai");
+    var textoEncriptado = textoEncriptado.replace(/o/img, "ober");
+    var textoEncriptado = textoEncriptado.replace(/u/img, "ufat");
+
+    document.getElementById("textoSalida").innerHTML = textoEncriptado;
+    
+}
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar();
+    textoIngreso.value = "";
+    munheco.style.visibility = "hidden";
+    botonCopiar.style.visibility = "visible";
+    instruccion.style.visibility = "hidden";
+    sinmensaje.style.visibility = "hidden";
+    
+}
+
+
+function desencriptar(){
+    var textoEntrada = document.getElementById("textoIngreso").value.toLowerCase();
+    var textoDesencriptado = textoEntrada.replace(/enter/img, "e");
+    var textoDesencriptado = textoDesencriptado.replace(/imes/img, "i");
+    var textoDesencriptado = textoDesencriptado.replace(/ai/img, "a");
+    var textoDesencriptado = textoDesencriptado.replace(/ober/img, "o");
+    var textoDesencriptado = textoDesencriptado.replace(/ufat/img, "u");
+
+    document.getElementById("textoSalida").innerHTML = textoDesencriptado; 
+}
+
+function btnDesencriptar(){
+    const textoDesencriptado = desencriptar();
+
+}
+
+
+function copiar(){
+    var contenido = document.querySelector("#textoSalida");
+    contenido.select();
+    document.execCommand("copy");
+}
+
+var btn3 = document.querySelector("#botonCopiar");
+btn3.onclick = copiar;
